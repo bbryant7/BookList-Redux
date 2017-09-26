c9a69431c325dc916799c8181c406d6d7921d033
Bailey Bryant <bailey.bryant10@gmail.com>
2017-09-26 14:12:35 -0500

    include all files, download dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+//react imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+//import promise from 'redux-promise';
+//react router imports
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//components
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+//switch uses most specific route that matches, top down.
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('#root'));
